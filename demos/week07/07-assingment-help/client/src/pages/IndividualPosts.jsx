cabf3e1ee6ad7340790dc13b11d4cd806f05e253
Sam Louloudis <samanthaleelouloudis@gmail.com>
2025-10-16 16:18:44 +0100

    add hidden field

--- demos/week07/07-assingment-help/client/src/pages/IndividualPosts.jsx
+++ demos/week07/07-assingment-help/client/src/pages/IndividualPosts.jsx
@@ -3,12 +3,14 @@ import { useParams, useSearchParams } from "react-router";
 
 export default function IndividualPosts() {
   const [postInfo, setPostInfo] = useState({});
-  // const { id } = useParams()
+  const [formData, setFormData] = useState({})
   const params = useParams();
   const postId = params.id;
-    console.log(postInfo)
 
-    
+
+    console.log(formData)
+
+
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(`http://localhost:8080/posts/${postId}`);
@@ -17,6 +19,12 @@ export default function IndividualPosts() {
     }
     fetchData();
   }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(e.target.postId.value)
+    setFormData({...formData, postId: e.target.postId.value})
+  }
 
   return (
     <div>
@@ -29,6 +37,11 @@ export default function IndividualPosts() {
       ) : (
         <p>No post was found</p>
       )}
+
+      <form onSubmit={handleSubmit}>
+        <input name="postId" type="hidden" value={postId} />
+        <input name="comment" onChange={(e) => setFormData({comment: e.target.value})}/>
+      </form>
     </div>
   );
 }
