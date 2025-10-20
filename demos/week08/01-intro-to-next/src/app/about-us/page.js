54606e3dcc4386b2ce6255751cb94c70d481cf41
Sam Louloudis <samanthaleelouloudis@gmail.com>
2025-10-20 10:29:35 +0100

    next demo

---
+++ demos/week08/01-intro-to-next/src/app/about-us/page.js
@@ -0,0 +1,11 @@
+// we must give next.js files called - page.js to render a page at that location
+
+// http://localhost:3000
+
+export default function Page() {
+    return (
+        <div>
+            <h2>This is my about page</h2>
+        </div>
+    )
+}
---
+++ demos/week08/01-intro-to-next/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+// this component is called home - but it lives in a file called page.js
+export default function Home() {
+  return (
+    <div>
+      <h2>This is kinda just like normal react!</h2>
+    </div>
+  );
+}
